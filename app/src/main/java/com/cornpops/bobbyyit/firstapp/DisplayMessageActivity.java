4ca9a938ad83ed19ef12b98b37b03c0a72cd6b3e
Bobby Yit <bobby.yit@gmail.com>
2015-02-17 22:50:11 -0500

    create action bar

--- app/src/main/java/com/cornpops/bobbyyit/firstapp/DisplayMessageActivity.java
+++ app/src/main/java/com/cornpops/bobbyyit/firstapp/DisplayMessageActivity.java
@@ -5,12 +5,10 @@ import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
 
 public class DisplayMessageActivity extends ActionBarActivity {
 
